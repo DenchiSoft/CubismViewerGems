539aa5ee1811d14d1c6842aed3b13ba55721c124
DenchiSoft <denchisoft@gmail.com>
2017-08-01 21:46:40 +0200

    Added gem to view/override parameters.

---
+++ Gems/Animating/CubismParameterInfo.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using Live2D.Cubism.Core;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class contains all information about values and UI elements related
+/// to one CubismParameter.
+/// </summary>
+public class CubismParameterInfo
+{
+	/// <summary>
+	/// The cubism parameter.
+	/// </summary>
+	public CubismParameter Parameter { get; set; }
+
+	/// <summary>
+	/// Whether or not the parameter is currently being overridden by the user.
+	/// </summary>
+	public bool Active { get; set; }
+
+	/// <summary>
+	/// The parameter value slider UI element.
+	/// </summary>
+	public Slider Slider { get; set; }
+
+	/// <summary>
+	/// The override toggle UI element.
+	/// </summary>
+	public Toggle Toggle { get; set; }
+
+	/// <summary>
+	/// The text field UI element that shows the current parameter value.
+	/// </summary>
+	public Text ValueText { get; set; }
+
+	/// <summary>
+	/// The background image of the parameter list entry. Indicates override state.
+	/// </summary>
+	public Image BackgroundTint { get; set; }
+
+	/// <summary>
+	/// Whether or not the last value was set by the animation.
+	/// Needed because the manual call to change the slider value also calls the slider value changed callback.
+	/// </summary>
+	public bool ValueSetByAnimation { get; set; }
+
+	/// <summary>
+	/// The override value set by the user
+	/// </summary>
+	public float OverrideVal { get; set; }
+
+	/// <summary>
+	/// CubismParameterInfo Constructor.
+	/// </summary>
+	public CubismParameterInfo(CubismParameter parameter, Toggle toggle, Text valueText, Image backgroundTint,
+			Slider slider, bool active, bool valueSetByAnimation, float overrideVal) {
+		this.Parameter = parameter;
+		this.Toggle = toggle;
+		this.ValueText = valueText;
+		this.BackgroundTint = backgroundTint;
+		this.Slider = slider;
+		this.Active = active;
+		this.ValueSetByAnimation = valueSetByAnimation;
+		this.OverrideVal = overrideVal;
+	}
+
+}
---
+++ Gems/Animating/ParamSliders.cs
@@ -0,0 +1,171 @@
+﻿ using UnityEngine;
+using Live2D.Cubism.Core;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+namespace Live2D.Cubism.Viewer.Gems.Animating
+{
+	/// <summary>
+	/// Shows available parameters and lets you control them.
+	/// </summary>
+	public sealed class ParamSliders : MonoBehaviour
+	{
+		// Cubism viewer.
+		private CubismViewer viewer;
+
+		// List containing all value/UI related information for each CubismParameter.
+		private List<CubismParameterInfo> CubismParamsInfo;
+
+		/// <summary>
+		/// Called by Unity. 
+		/// </summary>
+		private void Start()
+		{
+			var viewer = GetComponent<CubismViewer>();
+
+			// Fail silently in release.
+			if (viewer == null)
+			{
+				Debug.LogWarning("Not attached to viewer!");
+				return;
+			}
+
+			// Listeners for button click and new model event.
+			Button resetOverrideButton = GameObject.Find("ResetOverrideButton").GetComponent<Button>();
+			resetOverrideButton.onClick.AddListener(delegate {ResetOverrideClicked(); });
+
+			viewer.OnNewModel += OnNewModel;
+		}
+
+		/// <summary>
+		/// Called when a new Model is loaded.
+		/// </summary>
+		/// <param name="sender">The Sender/CubismViewer.</param>
+		/// <param name="model">The new Model.</param>
+		private void OnNewModel(CubismViewer sender, CubismModel model) {
+			// Check if old model is currently loaded.
+			if (CubismParamsInfo != null) {
+				// Destroy all old UI elements if they exist.
+				foreach (CubismParameterInfo param in CubismParamsInfo) {
+					GameObject.Destroy(param.Slider.gameObject.transform.parent.gameObject);
+				}
+			}
+
+			// Get template for parameter entries (find over parent because it's not enabled)
+			GameObject paramEntryTemplate = GameObject.Find("paramScroll").transform.Find("paramEntryTemplate").gameObject;
+
+			// Get scroll view content box. Parameter sliders are instantiated inside of this.
+			GameObject paramScrollContent = GameObject.Find("paramScrollContent");
+
+			CubismParamsInfo = new List<CubismParameterInfo>();
+
+			// Populate parameter UI scroll view.
+			foreach (CubismParameter p in model.Parameters) {
+				// Instantiate from template.
+				GameObject newParam = (GameObject)Instantiate(paramEntryTemplate);
+				newParam.transform.SetParent(paramScrollContent.transform);
+				newParam.SetActive(true);
+				newParam.name = p.Id;
+
+				// Set slider values.
+				Slider s = newParam.GetComponentInChildren<Slider>();
+				s.maxValue = p.MaximumValue;
+				s.minValue = p.MinimumValue;
+				s.value = p.Value;
+
+				// Set text fields.
+				Text t = newParam.GetComponentsInChildren<Text>()[3];
+				newParam.GetComponentsInChildren<Text>()[0].text =p.Id;
+				newParam.GetComponentsInChildren<Text>()[1].text = p.MinimumValue.ToString();
+				newParam.GetComponentsInChildren<Text>()[2].text = p.MaximumValue.ToString();
+				t.text = p.Value.ToString();
+
+				Toggle to = newParam.GetComponentInChildren<Toggle>();
+				Image img = newParam.GetComponent<Image>();
+
+				// Create list of all CubismParameters and their respective UI elements/override state.
+				CubismParameterInfo param = new CubismParameterInfo(p, to, t, img, s, false, false, p.Value);
+				CubismParamsInfo.Add(param);
+
+				// Listeners for slider and toggle button.
+				to.onValueChanged.AddListener(delegate(bool newValue) {ParamActiveStatusChanged(newValue, param); });
+				s.onValueChanged.AddListener(delegate(float newValue) {ParamValueChanged(newValue, param); });
+			}
+
+			// HACK Manually set scroll content height to height of children. Correct way to do this?
+			int paramEntryHeight = (int) ((RectTransform) paramEntryTemplate.transform).rect.height * model.Parameters.Length;
+			((RectTransform) paramScrollContent.transform).sizeDelta = new Vector2(0, paramEntryHeight);
+
+		}
+
+
+		/// <summary>
+		/// Called when the reset override button is clicked.
+		/// Resets override state of all parameters.
+		/// </summary>
+		private void ResetOverrideClicked() {
+			if (CubismParamsInfo == null || CubismParamsInfo.Count == 0)
+				return;
+
+			// Call override toggle to false callback for all parameters.
+			foreach (CubismParameterInfo param in CubismParamsInfo) {
+				param.Toggle.isOn = false;
+			}
+		}
+
+		/// <summary>
+		/// Called when override button is toggled.
+		/// Also called when user presses the reset button or manually moves a slider.
+		/// </summary>
+		/// <param name="newValue">Checked or unchecked.</param>
+		/// <param name="param">The associated CubismParameterInfo.</param>
+		private void ParamActiveStatusChanged(bool newValue, CubismParameterInfo param) {
+			param.Active = newValue;
+			param.BackgroundTint.enabled = newValue;
+			param.OverrideVal = param.Parameter.Value;
+		}
+
+		/// <summary>
+		/// Called when the user or the animation changes the slider value.
+		/// </summary>
+		/// <param name="newValue">New value of the slider.</param>
+		/// <param name="param">The associated CubismParameterInfo.</param>
+		private void ParamValueChanged(float newValue, CubismParameterInfo param) {
+
+			// Check if the call came from LateUpdate().
+			if (!param.ValueSetByAnimation) {
+				// If not, the parameter is not considered in override mode.
+				param.Active = true;
+				param.OverrideVal = newValue;
+				param.Toggle.isOn = true;
+				param.BackgroundTint.enabled = true;
+			} else {
+				param.ValueSetByAnimation = false;
+			}
+		}
+
+		/// <summary>
+		/// Called by Unity. 
+		/// </summary>
+		void LateUpdate()
+		{
+			if (CubismParamsInfo == null || CubismParamsInfo.Count == 0)
+				return;
+
+			// Iterate over all parameters.
+			// If a paremeter is in override mode, set override value.
+			// If not, set value and indicate it has been set by the animation.
+			foreach (CubismParameterInfo param in CubismParamsInfo) {
+				if (param.Active) {
+					param.Parameter.Value = param.OverrideVal;
+					param.ValueText.text = System.Math.Round(param.OverrideVal, 2).ToString();
+				} else {
+					param.ValueSetByAnimation = true;
+					param.Slider.value = param.Parameter.Value;
+					param.ValueText.text = System.Math.Round(param.Parameter.Value, 2).ToString();
+				}
+			}
+		}
+	}
+}
